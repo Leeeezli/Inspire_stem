3618dba8b5d5a369cd941991974b7c88fb5a1a15
Lesley <159755331+Leeeezli@users.noreply.github.com>
2024-02-21 09:42:32 +0300

    Update geometric_progression.py

--- geometric_progression.py
+++ geometric_progression.py
@@ -6,8 +6,8 @@ a = float(input("Enter the value of a : "))
 n = float(input("Enter the value of n  :"))
 r = float(input("Enter the value of r :"))
 
-S_n = (a*(r**n-1)/(r-1))
+S_n = (a * (r * * n - 1)/(r - 1))
 print("The sum of the numbers is ",S_n)
 
-nth = a*r**(n-1)
+nth = a * r * *(n - 1)
 print("The nth term is ",nth)
