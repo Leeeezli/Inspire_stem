6ab35152d4d8f4cc4f53839895040e548b6b6029
Leeeezli <wawerulesley26@gmail.com>
2024-03-20 10:17:07 +0300

    upto guess.js

--- Week_5/web/guess_game/guess.js
+++ Week_5/web/guess_game/guess.js
@@ -1,1 +1,22 @@
-document.getElementById("message").innerHTML="Message here"
+const randomNumber = math.floor(Math.random() * 100) + 1 ;
+let attempt = 0 ;
+
+function checkGuess(){
+    const guess = parseint(document.getElementById("guessField").value)
+    attempt++;
+    if(isNaN(guess) || guess < 1 || guess > 100){
+        setMesage("Please enter a valid number between 1 and 100")
+        return;
+    }
+    if (guess === randomNumber){
+        setMesage("Congratulation ! Guessed correctly")
+        document.getElementById("guessField").disabled = true;
+    }else if(guess < randomNumber){
+        setMessage("Too low try again")
+    }else(
+        setMessage("Too hogh try again")
+    )
+}
+function setMessage(message){
+    document.getElementById("message").textContent = message;
+}
