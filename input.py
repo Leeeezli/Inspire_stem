188ce141b366dc6a0a98502ed53c367f38a6bfe2
Lesley <159755331+Leeeezli@users.noreply.github.com>
2024-03-22 09:58:09 +0300

    Delete input.py

--- input.py
+++
@@ -1,8 +0,0 @@
-# program to get user input
-# date : 20/02/2024
-# name : Lesley Waweru
-
-f_name = input("enter your first name")
-s_name = input("enter your second name")
-
-print("my name is ",f_name + s_name)
